1f742705d921ef3c99590913e3de064fee475990
stevenliuyi <stevenliuyi@gmail.com>
2018-08-18 04:26:43 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ class App extends Component {
   render() {
     return (
       <div className="background">
-        <Typist className="title" startDelay={1000}>
-          <span>hi, i'm Yi Liu</span>
+        <Typist className="title" startDelay={1500} avgTypingDelay={100}>
+          <span>hi, I'm Yi Liu</span>
           <Typist.Backspace count={6} delay={1000} />
           <span>Steven.</span>
         </Typist>
         <div className="main-links">
           <a href="https://blog.yliu.io">
-            <span className="main-link"><TiPencil size={14} className="main-icon"/> blog</span>
+            <span className="main-link">
+              <TiPencil size={14} className="main-icon"/>
+              <span className="main-link-text">blog</span>
+            </span>
           </a>
           <a href="https://github.com/stevenliuyi">
-            <span className="main-link"><TiCode size={14} className="main-icon" /> github</span>
+            <span className="main-link">
+              <TiCode size={14} className="main-icon" />
+              <span className="main-link-text">github</span>
+            </span>
           </a>
         </div>
       </div>
