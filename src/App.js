a09b3f05cb9a7d688d66b4edeffb6aca1b1037a1
stevenliuyi <stevenliuyi@gmail.com>
2018-08-20 21:01:17 -0400

    add placeholder contents & scrolling animations

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 import Typist from 'react-typist'
 import 'react-typist/dist/Typist.css'
 import 'babel-polyfill'
+import ScrollAnim from 'rc-scroll-anim'
+import QueueAnim from 'rc-queue-anim'
+import TweenOne from 'rc-tween-one'
+import Animate from 'rc-animate'
 import { TiPencil, TiCode } from 'react-icons/ti'
 
+const Link = ScrollAnim.Link
+const Element = ScrollAnim.Element
+const ScrollOverPack = ScrollAnim.OverPack
+const EventListener = ScrollAnim.Event
+
 class App extends Component {
+
   state = {
     linkShown: false
   }
@@ -19,33 +29,107 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    EventListener.addEventListener('resize.userResize', this.barAnimate.bind(this))
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    let nav = document.querySelector('.nav')
+    let el = document.scrollingElement || document.documentElement
+    if (el.scrollTop > window.innerHeight) {
+      nav.style.position = 'fixed'
+      nav.style.top = 0
+    } else {
+      nav.style.position = 'static'
+      nav.style.top = 'auto'
+    }
+  }
+
+  onFocus = (e) => {
+    this.dom = e.target
+    this.barAnimate()
+  }
+
+  barAnimate = () => {
+    if (!this.dom) {
+      return;
+    }
+    const bar = this.refs.bar;
+    bar.style.left = `${this.dom.getBoundingClientRect().left}px`;
+  }
+
   render() {
     return (
-      <div className="background">
-        <Typist
-          className="title"
-          startDelay={1200}
-          avgTypingDelay={100}
-          onCharacterTyped={() => {if (!this.state.linkShown) this.setState({ linkShown: true })}}
-        >
-          <span>hi, I'm Yi Liu</span>
-          <Typist.Backspace count={6} delay={1000} />
-          <span>Steven.</span>
-        </Typist>
-        <div className="main-links">
-          <a href="https://blog.yliu.io">
-            <span className="main-link">
-              <TiPencil size={14} className="main-icon"/>
-              <span className="main-link-text">blog</span>
-            </span>
-          </a>
-          <a href="https://github.com/stevenliuyi">
-            <span className="main-link">
-              <TiCode size={14} className="main-icon" />
-              <span className="main-link-text">github</span>
-            </span>
-          </a>
+      <div>
+        <Element className="background">
+          <Typist
+            className="title"
+            startDelay={1200}
+            avgTypingDelay={100}
+            onCharacterTyped={() => {if (!this.state.linkShown) this.setState({ linkShown: true })}}
+          >
+            <span>hi, I'm Yi Liu</span>
+            <Typist.Backspace count={6} delay={1000} />
+            <span>Steven.</span>
+          </Typist>
+          <div className="main-links">
+            <a href="https://blog.yliu.io">
+              <span className="main-link">
+                <TiPencil size={14} className="main-icon"/>
+                <span className="main-link-text">blog</span>
+              </span>
+            </a>
+            <a href="https://github.com/stevenliuyi">
+              <span className="main-link">
+                <TiCode size={14} className="main-icon" />
+                <span className="main-link-text">github</span>
+              </span>
+            </a>
+          </div>
+        </Element>
+        <div className="nav">
+          <div className="nav-wrap">
+            <Link className="nav-list" to="about-page" onFocus={this.onFocus}>About</Link>
+            <Link className="nav-list" to="project-page" onFocus={this.onFocus}>Projects</Link>
+            <Link className="nav-list" to="contact-page" onFocus={this.onFocus}>Contact</Link>
+            <div ref="bar" className="nav-bar" />
+          </div>
         </div>
+        <ScrollOverPack id="about-page" className="about-page" playScale={0.4}>
+          <QueueAnim type="left" key='0' duration={1000} interval={200} leaveReverse={true}>
+            <div key='0' className="section-title">ABOUT</div>
+            <div key='1' className="underline" />
+          </QueueAnim>
+          <TweenOne key='1' style={{ opacity: 0, transform: 'rotateY(90deg)'}} animation={{ opacity: 1, rotateY: 0 ,  duration:1000, ease: 'easeInQuart' }}>
+            <img className="logo" src="/icons/android-chrome-192x192.png" height={100} width={100} />
+          </TweenOne>
+          <QueueAnim type="left" key='2' className="about-text" duration={1000} interval={200} delay={[1000,0]} leaveReverse={true}>
+            <div key='0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum est id diam ullamcorper cursus. Aenean pharetra dolor eget lorem scelerisque, quis blandit justo finibus. Aliquam a pulvinar nisl. Proin blandit velit sed ipsum pellentesque, ut feugiat enim volutpat. Curabitur turpis lectus, aliquet vitae ante a, facilisis consequat quam. </div>
+            <div key='1'>Nulla a egestas nibh. Suspendisse hendrerit magna sed odio dignissim blandit. Morbi pharetra mi non ullamcorper semper. Vestibulum mattis, est eget condimentum blandit, est eros tristique diam, sit amet mattis tellus enim quis augue. Sed sed justo tempus, auctor neque et, placerat nulla. Ut euismod fringilla diam nec varius.</div>
+            <div key='2'>Etiam non tortor diam. Aliquam et tempus velit. Nam interdum nunc nec tortor gravida egestas. Curabitur porttitor sodales ante ut ullamcorper.</div>
+          </QueueAnim>
+        </ScrollOverPack>
+        <ScrollOverPack id="project-page" className="project-page" playScale={0.4}>
+          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={[0,1000]} leaveReverse={true}>
+            <div key='0' className="section-title">PROJECTS</div>
+            <div key='1' className="underline" />
+          </QueueAnim>
+          <QueueAnim animConfig={{rotateX:[0,90]}} key='1' className="projects" duration={2000} interval={0} delay={[1000, 0]}>
+            <div key="0" className="project-card">PROJECT 1</div>
+            <div key="1" className="project-card">PROJECT 2</div>
+            <div key="2" className="project-card">PROJECT 3</div>
+            <div key="3" className="project-card">PROJECT 4</div>
+            <div key="4" className="project-card">PROJECT 5</div>
+            <div key="5" className="project-card">PROJECT 6</div>
+          </QueueAnim>
+        </ScrollOverPack>
+        <ScrollOverPack id="contact-page" className="contact-page" playScale={0.1}>
+          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={[0,1000]} leaveReverse={true}>
+            <div key='0' className="section-title white">THANKS</div>
+            <div key='1' className="underline white" />
+          </QueueAnim>
+        </ScrollOverPack>
       </div>
     );
   }
