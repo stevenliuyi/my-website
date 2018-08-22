b4ff8274183373f4ad62c3da02c339a54ab378e6
stevenliuyi <stevenliuyi@gmail.com>
2018-08-22 02:07:03 -0400

    delay animation and only play once

--- src/App.js
+++ src/App.js
@@ -131,32 +131,32 @@ class App extends Component {
             <div ref="bar" className="nav-bar" />
           </div>
         </div>
-        <ScrollOverPack ref={el => this.aboutPage = el} id="about-page" className="about-page" playScale={0.4}>
-          <QueueAnim type="left" key='0' duration={1000} interval={200} leaveReverse={true}>
+        <ScrollOverPack ref={el => this.aboutPage = el} id="about-page" className="about-page" playScale={0.6} always={false}>
+          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={500}>
             <div key='0' className="section-title">ABOUT</div>
             <div key='1' className="underline" />
           </QueueAnim>
-          <TweenOne key='1' style={{ opacity: 0, transform: 'rotateY(90deg)'}} animation={{ opacity: 1, rotateY: 0 ,  duration:1000, ease: 'easeInQuart' }}>
+          <TweenOne key='1' style={{ opacity: 0, transform: 'rotateY(90deg)'}} animation={{ opacity: 1, rotateY: 0 ,  duration:1000, ease: 'easeInQuart', delay: 500 }}>
             <img className="logo" src="/icons/android-chrome-192x192.png" height={100} width={100} alt="logo" />
           </TweenOne>
-          <QueueAnim animConfig={{opacity:[1,0], translateX: [0, -100]}} key='2' className="about-text" duration={1000} interval={200} delay={[1000,0]} leaveReverse={true}>
+          <QueueAnim animConfig={{opacity:[1,0], translateX: [0, -100]}} key='2' className="about-text" duration={1000} interval={200} delay={1500}>
             <div key='0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum est id diam ullamcorper cursus. Aenean pharetra dolor eget lorem scelerisque, quis blandit justo finibus. Aliquam a pulvinar nisl. Proin blandit velit sed ipsum pellentesque, ut feugiat enim volutpat. Curabitur turpis lectus, aliquet vitae ante a, facilisis consequat quam. </div>
             <div key='1'>Nulla a egestas nibh. Suspendisse hendrerit magna sed odio dignissim blandit. Morbi pharetra mi non ullamcorper semper. Vestibulum mattis, est eget condimentum blandit, est eros tristique diam, sit amet mattis tellus enim quis augue. Sed sed justo tempus, auctor neque et, placerat nulla. Ut euismod fringilla diam nec varius.</div>
             <div key='2'>Etiam non tortor diam. Aliquam et tempus velit. Nam interdum nunc nec tortor gravida egestas. Curabitur porttitor sodales ante ut ullamcorper.</div>
           </QueueAnim>
-          <TweenOne key='3' className="number-cards" style={{ opacity: 0, transform: 'translateY(50px)'}} animation={{ opacity: 1, translateY: 0, duration: 1000, delay: 2000 }}>
+          <TweenOne key='3' className="number-cards" style={{ opacity: 0, transform: 'translateY(50px)'}} animation={{ opacity: 1, translateY: 0, duration: 1000, delay: 2500 }}>
             <NumberCard description="number of Wikipedia articles I created" number="20,000" detail="These articles have been viewed 15 million times since 2015." />
             <NumberCard description="my Erdős number" number="6" detail={<div><a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Erdős_number">Erdős number</a> is one's collabration distance to mathematican Paul Erdös.</div>} />
             <NumberCard description="number of U.S. states I've set foot on" number="23" detail="... and Washington, D.C.!" />
             <NumberCard description="my favorite number" number="42" detail={<div>Because it's <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Phrases_from_The_Hitchhiker%27s_Guide_to_the_Galaxy#Answer_to_the_Ultimate_Question_of_Life,_the_Universe,_and_Everything_(42)">the Answer to the Ultimate  Question of Life, the Universe, and Everything</a>!</div>}/>
           </TweenOne>
         </ScrollOverPack>
-        <ScrollOverPack id="project-page" className="project-page" playScale={0.4}>
-          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={[0,1000]} leaveReverse={true}>
+        <ScrollOverPack id="project-page" className="project-page" playScale={0.6} always={false}>
+          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={500} leaveReverse={true}>
             <div key='0' className="section-title">PROJECTS</div>
             <div key='1' className="underline" />
           </QueueAnim>
-          <QueueAnim type="bottom" key='1' className="projects" duration={2000} interval={0} delay={[1000, 0]}
+          <QueueAnim type="bottom" key='1' className="projects" duration={2000} interval={0} delay={1500}
            onEnd={({ key, type }) => {
              this.setState({ projectsLoaded: type === 'enter' ? true : false })
            }}
@@ -176,7 +176,7 @@ class App extends Component {
               >
                 <TweenOne
                   style={{opacity: 0, transform: 'translateX(-50px)'}}
-                  animation={{opacity: 1, x: 0, duration: 1000, ease: 'easeOutQuart', delay: 500}}
+                  animation={{opacity: 1, x: 0, duration: 1000, ease: 'easeOutQuart', delay: 1000}}
                 >
                   <CardTitle>{`Project ${i+1}`}</CardTitle>
                   <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</CardText>
@@ -190,8 +190,8 @@ class App extends Component {
         <div className="scroll-to-top" onClick={() => scrollToComponent(this.top, { align: 'top', duration: 500 })}>
           <div className="scroll-arrow"><FaAngleDoubleUp size={24} /></div>
         </div>
-        <ScrollOverPack id="contact-page" className="contact-page" playScale={0.1}>
-          <QueueAnim type="left" key='0' duration={1000} interval={200} leaveReverse={true}>
+        <ScrollOverPack id="contact-page" className="contact-page" playScale={0.1} always={false}>
+          <QueueAnim type="left" key='0' duration={1000} delay={500} interval={200}>
             <div key='0' className="section-title" style={{color: '#fff'}}>THANKS</div>
             <div key='1' className="underline" style={{borderTopColor: '#fff'}} />
           </QueueAnim>
