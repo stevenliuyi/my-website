61bacd1c72f2e9857d7ead20cd79d106940c27de
stevenliuyi <stevenliuyi@gmail.com>
2018-08-22 03:06:04 -0400

    add text animations; replace queue-anim with tweenone and more

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Typist from 'react-typist'
 import 'react-typist/dist/Typist.css'
 import 'babel-polyfill'
 import ScrollAnim from 'rc-scroll-anim'
-import QueueAnim from 'rc-queue-anim'
 import TweenOne from 'rc-tween-one'
+import Texty from 'rc-texty'
 import { TiPencil, TiCode } from 'react-icons/ti'
 import { IoIosArrowDown } from 'react-icons/io'
 import { FaAngleDoubleUp } from 'react-icons/fa'
@@ -30,10 +30,16 @@ class App extends Component {
 
   componentDidUpdate({ linkShown}) {
     if (!linkShown && this.state.linkShown) {
-            let elem = document.querySelector('.main-links')
-            elem.style.opacity = 1
-            elem.style.marginTop = '50px'
-
+      ['main-links', 'more-text', 'arrow-down'].forEach(showClass => {
+        let elem = document.querySelector(`.${showClass}`)
+        elem.style.opacity = 1
+        elem.style.transform = 'translate(-50%, 0)'
+        if (showClass === 'arrow-down') {
+          const animation = 'arrow-down-flash linear 1.5s infinite'
+          elem.style.animation = animation
+          elem.style.WebkitAnimation = animation
+        }
+      })      
     }
   }
 
@@ -131,19 +137,23 @@ class App extends Component {
             <div ref="bar" className="nav-bar" />
           </div>
         </div>
-        <ScrollOverPack ref={el => this.aboutPage = el} id="about-page" className="about-page" playScale={0.6} always={false}>
-          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={500}>
-            <div key='0' className="section-title">ABOUT</div>
-            <div key='1' className="underline" />
-          </QueueAnim>
-          <TweenOne key='1' style={{ opacity: 0, transform: 'rotateY(90deg)'}} animation={{ opacity: 1, rotateY: 0 ,  duration:1000, ease: 'easeInQuart', delay: 500 }}>
+        <ScrollOverPack ref={el => this.aboutPage = el} id="about-page" className="about-page" playScale={0.5} always={false}>
+          <Texty key='0' type="left" mode="smooth" className="section-title" delay={500}>ABOUT</Texty>
+          <TweenOne key='1' className="underline" style={{ opacity: 0, transform: 'translateX(-50px)'}} animation={{ opacity: 1, translateX: 0, delay: 750, duration: 1000}} />
+          <TweenOne key='2' style={{ opacity: 0, transform: 'rotateY(90deg)'}} animation={{ opacity: 1, rotateY: 0 ,  duration:1000, ease: 'easeInQuart', delay: 500 }}>
             <img className="logo" src="/icons/android-chrome-192x192.png" height={100} width={100} alt="logo" />
           </TweenOne>
-          <QueueAnim animConfig={{opacity:[1,0], translateX: [0, -100]}} key='2' className="about-text" duration={1000} interval={200} delay={1500}>
-            <div key='0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum est id diam ullamcorper cursus. Aenean pharetra dolor eget lorem scelerisque, quis blandit justo finibus. Aliquam a pulvinar nisl. Proin blandit velit sed ipsum pellentesque, ut feugiat enim volutpat. Curabitur turpis lectus, aliquet vitae ante a, facilisis consequat quam. </div>
-            <div key='1'>Nulla a egestas nibh. Suspendisse hendrerit magna sed odio dignissim blandit. Morbi pharetra mi non ullamcorper semper. Vestibulum mattis, est eget condimentum blandit, est eros tristique diam, sit amet mattis tellus enim quis augue. Sed sed justo tempus, auctor neque et, placerat nulla. Ut euismod fringilla diam nec varius.</div>
-            <div key='2'>Etiam non tortor diam. Aliquam et tempus velit. Nam interdum nunc nec tortor gravida egestas. Curabitur porttitor sodales ante ut ullamcorper.</div>
-          </QueueAnim>
+          <div className="about-text">
+            <TweenOne key='0' style={{ opacity: 0, transform: 'translateX(-100px)'}} animation={{ opacity: 1, translateX: 0, delay: 1500, duration: 1000}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum est id diam ullamcorper cursus. Aenean pharetra dolor eget lorem scelerisque, quis blandit justo finibus. Aliquam a pulvinar nisl. Proin blandit velit sed ipsum pellentesque, ut feugiat enim volutpat. Curabitur turpis lectus, aliquet vitae ante a, facilisis consequat quam. 
+            </TweenOne>
+            <TweenOne key='1' style={{ opacity: 0, transform: 'translateX(-100px)'}} animation={{ opacity: 1, translateX: 0, delay: 1700, duration: 1000}}>
+              Nulla a egestas nibh. Suspendisse hendrerit magna sed odio dignissim blandit. Morbi pharetra mi non ullamcorper semper. Vestibulum mattis, est eget condimentum blandit, est eros tristique diam, sit amet mattis tellus enim quis augue. Sed sed justo tempus, auctor neque et, placerat nulla. Ut euismod fringilla diam nec varius.
+            </TweenOne>
+            <TweenOne key='2' style={{ opacity: 0, transform: 'translateX(-100px)'}} animation={{ opacity: 1, translateX: 0, delay: 1900, duration: 1000}}>
+              Etiam non tortor diam. Aliquam et tempus velit. Nam interdum nunc nec tortor gravida egestas. Curabitur porttitor sodales ante ut ullamcorper.
+            </TweenOne>
+          </div>
           <TweenOne key='3' className="number-cards" style={{ opacity: 0, transform: 'translateY(50px)'}} animation={{ opacity: 1, translateY: 0, duration: 1000, delay: 2500 }}>
             <NumberCard description="number of Wikipedia articles I created" number="20,000" detail="These articles have been viewed 15 million times since 2015." />
             <NumberCard description="my Erdős number" number="6" detail={<div><a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Erdős_number">Erdős number</a> is one's collabration distance to mathematican Paul Erdös.</div>} />
@@ -151,14 +161,12 @@ class App extends Component {
             <NumberCard description="my favorite number" number="42" detail={<div>Because it's <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Phrases_from_The_Hitchhiker%27s_Guide_to_the_Galaxy#Answer_to_the_Ultimate_Question_of_Life,_the_Universe,_and_Everything_(42)">the Answer to the Ultimate  Question of Life, the Universe, and Everything</a>!</div>}/>
           </TweenOne>
         </ScrollOverPack>
-        <ScrollOverPack id="project-page" className="project-page" playScale={0.6} always={false}>
-          <QueueAnim type="left" key='0' duration={1000} interval={200} delay={500} leaveReverse={true}>
-            <div key='0' className="section-title">PROJECTS</div>
-            <div key='1' className="underline" />
-          </QueueAnim>
-          <QueueAnim type="bottom" key='1' className="projects" duration={2000} interval={0} delay={1500}
-           onEnd={({ key, type }) => {
-             this.setState({ projectsLoaded: type === 'enter' ? true : false })
+        <ScrollOverPack id="project-page" className="project-page" playScale={0.5} always={false}>
+          <Texty key='0' type="left" mode="smooth" className="section-title" delay={500}>PROJECTS</Texty>
+          <TweenOne key='1' className="underline" style={{ opacity: 0, transform: 'translateX(-50px)'}} animation={{ opacity: 1, translateX: 0, delay: 750, duration: 1000}} />
+          <TweenOne key='2' className="projects" style={{ opacity: 0, transform: 'translateY(100px)' }} animation={{ opacity: 1, translateY: 0, delay: 1500, duration: 2000}}
+           onComplete={(e) => {
+             this.setState({ projectsLoaded: true })
            }}
           >
           { [...Array(6).keys()].map(i => (
@@ -185,16 +193,14 @@ class App extends Component {
             </Card>
           ))
           }
-          </QueueAnim>
+          </TweenOne>
         </ScrollOverPack>
         <div className="scroll-to-top" onClick={() => scrollToComponent(this.top, { align: 'top', duration: 500 })}>
           <div className="scroll-arrow"><FaAngleDoubleUp size={24} /></div>
         </div>
         <ScrollOverPack id="contact-page" className="contact-page" playScale={0.1} always={false}>
-          <QueueAnim type="left" key='0' duration={1000} delay={500} interval={200}>
-            <div key='0' className="section-title" style={{color: '#fff'}}>THANKS</div>
-            <div key='1' className="underline" style={{borderTopColor: '#fff'}} />
-          </QueueAnim>
+          <Texty key='0' type="left" mode="smooth" className="section-title" delay={500} style={{color: '#fff'}}>THANKS</Texty>
+          <TweenOne key='1' className="underline" style={{ opacity: 0, transform: 'translateX(-50px)', borderTopColor: '#fff' }} animation={{ opacity: 1, translateX: 0, delay: 750, duration: 1000}} />
         </ScrollOverPack>
       </div>
     );
