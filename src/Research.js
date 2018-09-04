aff7b6f6cc6bd610c439b1af118770ceb6782487
stevenliuyi <stevenliuyi@gmail.com>
2018-09-04 14:31:23 -0400

    research page improvements

--- src/Research.js
+++ src/Research.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ScrollAnim from 'rc-scroll-anim'
 import TweenOne from 'rc-tween-one'
 import Texty from 'rc-texty'
-import { Badge } from 'reactstrap'
+import { Badge, Button } from 'reactstrap'
 
 const ScrollOverPack = ScrollAnim.OverPack
 
@@ -15,7 +15,7 @@ class Research extends Component {
         <TweenOne key='1' className="underline" animation={{ opacity: 1, translateX: 0, delay: this.props.delay + 250, duration: 1000}} />
         <div className="keywords-wrap">
           <TweenOne key='0' animation={{ opacity: 1, translateX: 0, delay: this.props.delay + 500}} className="keywords-title">KEYWORDS</TweenOne>
-          <TweenOne key='1' animation={{ opacity: 1, delay: this.props.delay + 700}} component={Badge} color="secondary" className="keyword">computational fluid mechanics</TweenOne>
+          <TweenOne key='1' animation={{ opacity: 1, delay: this.props.delay + 700}} component={Badge} color="secondary" className="keyword">computational fluid dynamics</TweenOne>
           <TweenOne key='2' animation={{ opacity: 1, delay: this.props.delay + 900}} component={Badge} color="secondary" className="keyword">turbulence</TweenOne>
           <TweenOne key='3' animation={{ opacity: 1, delay: this.props.delay + 1100}} component={Badge} color="secondary" className="keyword">high performance computing</TweenOne>
         </div>
@@ -25,8 +25,20 @@ class Research extends Component {
               <img className="research-image" src="/images/gulf_stream.png" alt="gulf strem coherent structures" />
             </div>
             <div className="research-text">
-              <div className="research-title">Lagrangian coherent structures in the Gulf stream</div>
-              <div className="research-detai">The finite‐time Lyapunov exponent (FTLE) is calculated from satellite altimetry to identify Lagrangian coherent structures (LCS) in the Gulf Stream region to reexamine a picture of the transport and mixing processes in this region.</div>
+              <div className="research-title">Lagrangian coherent structures in the Gulf Stream</div>
+              <div className="research-detai">The finite-time Lyapunov exponent (FTLE) is calculated from satellite altimetry to identify Lagrangian coherent structures (LCS) in the Gulf Stream region to reexamine a picture of the transport and mixing processes in this region.</div>
+              <Button outline color="secondary" size="sm" className="research-button" onClick={() => {
+                window.open('https://agupubs.onlinelibrary.wiley.com/doi/10.1002/2017JC013390', 'blank_')
+              }}>Paper</Button>
+            </div>
+          </TweenOne>
+          <TweenOne key='3' className="research"  animation={{ opacity: 1, translateX: 0, delay: this.props.delay + 1500, duration: 1000}}>
+            <div className="research-image-wrap">
+              <img className="research-image" src="/images/channel_flow_q.png" alt="channel flow q criterion" />
+            </div>
+            <div className="research-text">
+              <div className="research-title">Wall pressure fluctuations in compressible turbulent channel flows</div>
+              <div className="research-detai">Direct numerical simulations (DNS) are performed using high-order non-dissipative schemes to investigate spatiotemporal characteristics of unsteady wall pressure in both subsonice and supersonice compressible channel flows.</div>
             </div>
           </TweenOne>
         </div>
