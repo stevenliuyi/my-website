9dcd01548bfef6cd6d81748d3a4194da123481c6
stevenliuyi <stevenliuyi@gmail.com>
2018-09-05 06:02:45 -0400

    add email

--- src/Footer.js
+++ src/Footer.js
@@ -12,8 +12,9 @@ class Footer extends Component {
   render() {
     return (
       <ScrollOverPack id="contact-page" className="contact-page" playScale={0.1} always={false}>
-        <Texty key='0' enter={textEnter} className="section-title" delay={this.props.delay}>THANKS</Texty>
+        <Texty key='0' enter={textEnter} className="section-title" delay={this.props.delay}>GET IN TOUCH</Texty>
         <TweenOne key='1' className="underline" animation={{ opacity: 1, translateX: 0, delay: this.props.delay + 250, duration: 1000}} />
+        <TweenOne key='2' className="contact-email" animation={{ opacity: 1, translateY: 0, delay: this.props.delay + 500, duration: 1000}}><a href="mailto:me@yliu.io">me<span className="at">@</span>yliu.io</a></TweenOne>
       </ScrollOverPack>
     );
   }
