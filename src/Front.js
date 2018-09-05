cb98604af4f2fcde941a47260daed050c749d696
stevenliuyi <stevenliuyi@gmail.com>
2018-09-05 03:49:54 -0400

    cover page scrolling effect

--- src/Front.js
+++ src/Front.js
@@ -85,6 +85,13 @@ class Front extends Component {
 
   componentDidMount() {
     this.snowy()
+
+    const ratio = 0.5
+    window.onscroll = (e) => {
+      const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+      const opacity = scrollTop > ratio * window.innerHeight ? 0 : 1 - scrollTop / window.innerHeight / ratio
+      document.querySelector('.background').style.opacity =  opacity
+    }
   }
 
   componentDidUpdate({ linkShown}) {
@@ -104,41 +111,44 @@ class Front extends Component {
 
   render() {
     return (
-      <div className="background">
-        <canvas id="snow"></canvas>
-        <ProgressiveImage
-          preview="/images/fabrice-villard-584622-unsplash-small.jpg"
-          src="/images/fabrice-villard-584622-unsplash.jpg"
-          render={(src, style) => <div className="background-image" style={Object.assign(style, { backgroundImage: `url(${src})`})} />}
-        />
-        <Typist
-          className="title"
-          startDelay={1200}
-          avgTypingDelay={100}
-          onCharacterTyped={() => {if (!this.state.linkShown) this.setState({ linkShown: true })}}
-        >
-          <span>hi, I'm Yi Liu</span>
-          <Typist.Backspace count={6} delay={1000} />
-          <span>Steven.</span>
-        </Typist>
-        <div className="main-links">
-          <a href="https://blog.yliu.io">
-            <span className="main-link">
-              <TiPencil size={14} className="main-icon"/>
-              <span className="main-link-text">blog</span>
-            </span>
-          </a>
-          <a href="https://github.com/stevenliuyi">
-            <span className="main-link">
-              <TiCode size={14} className="main-icon" />
-              <span className="main-link-text">github</span>
-            </span>
-          </a>
+      <div>
+        <div className="background">
+          <canvas id="snow"></canvas>
+          <ProgressiveImage
+            preview="/images/fabrice-villard-584622-unsplash-small.jpg"
+            src="/images/fabrice-villard-584622-unsplash.jpg"
+            render={(src, style) => <div className="background-image" style={Object.assign(style, { backgroundImage: `url(${src})`})} />}
+          />
+          <Typist
+            className="title"
+            startDelay={1200}
+            avgTypingDelay={100}
+            onCharacterTyped={() => {if (!this.state.linkShown) this.setState({ linkShown: true })}}
+          >
+            <span>hi, I'm Yi Liu</span>
+            <Typist.Backspace count={6} delay={1000} />
+            <span>Steven.</span>
+          </Typist>
+          <div className="main-links">
+            <a href="https://blog.yliu.io">
+              <span className="main-link">
+                <TiPencil size={14} className="main-icon"/>
+                <span className="main-link-text">blog</span>
+              </span>
+            </a>
+            <a href="https://github.com/stevenliuyi">
+              <span className="main-link">
+                <TiCode size={14} className="main-icon" />
+                <span className="main-link-text">github</span>
+              </span>
+            </a>
+          </div>
+          <div className="more-text" onClick={this.props.scrollToNav}>more</div>
+          <div className="arrow-down" onClick={this.props.scrollToNav}>
+            <IoIosArrowDown size={24} />
+          </div>
         </div>
-        <div className="more-text" onClick={this.props.scrollToNav}>more</div>
-        <div className="arrow-down" onClick={this.props.scrollToNav}>
-          <IoIosArrowDown size={24} />
-        </div>
+        <div className="background-empty"></div>
       </div>
     );
   }
