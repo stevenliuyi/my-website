2172e555b67bfb83b4af918eafdbaff5918ff9c3
stevenliuyi <stevenliuyi@gmail.com>
2018-09-23 14:29:52 -0400

    redirect shortened URLs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './components/App'
 import Read from './components/Read'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
@@ -11,6 +12,18 @@ ReactDOM.render(
     <div>
       <Route exact path="/" component={App} />
       <Route exact path="/read" component={Read} />
+      {/* redirect to https://go.yliu.io for shortened URLs */}
+      <Route
+        path="/:id"
+        component={({ match }) => (
+          <Helmet>
+            <meta
+              http-equiv="refresh"
+              content={`0;URL=https://go.yliu.io/${match.params.id}`}
+            />
+          </Helmet>
+        )}
+      />
     </div>
   </Router>,
   document.getElementById('root')
