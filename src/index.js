c0c179b7ef95f4d29847cefc309c6ae4181bc8bc
stevenliuyi <stevenliuyi@gmail.com>
2018-09-23 18:32:58 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -3,27 +3,29 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import Read from './components/Read'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path="/" component={App} />
-      <Route exact path="/read" component={Read} />
-      {/* redirect to https://go.yliu.io for shortened URLs */}
-      <Route
-        path="/:id"
-        component={({ match }) => (
-          <Helmet>
-            <meta
-              http-equiv="refresh"
-              content={`0;URL=https://go.yliu.io/${match.params.id}`}
-            />
-          </Helmet>
-        )}
-      />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/read" component={Read} />
+        {/* redirect to https://go.yliu.io for shortened URLs */}
+        <Route
+          path="/:id"
+          component={({ match }) => (
+            <Helmet>
+              <meta
+                http-equiv="refresh"
+                content={`0;URL=https://go.yliu.io/${match.params.id}`}
+              />
+            </Helmet>
+          )}
+        />
+      </Switch>
     </div>
   </Router>,
   document.getElementById('root')
