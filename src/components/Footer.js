25f2f0a6986ec78b2623022eb6916f150b195844
stevenliuyi <stevenliuyi@gmail.com>
2018-09-27 17:38:15 -0400

    add PGP key

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import TweenOne from 'rc-tween-one'
 import Texty from 'rc-texty'
 import 'rc-texty/assets/index.css'
 import { TiHeart } from 'react-icons/ti'
-import { FaFacebookF, FaWeixin } from 'react-icons/fa'
+import { FaFacebookF, FaWeixin, FaKey } from 'react-icons/fa'
+import { MdLock } from 'react-icons/md'
 import { Tooltip } from 'reactstrap'
 import textEnter from '../utils/textEnter'
 
@@ -12,7 +13,8 @@ const ScrollOverPack = ScrollAnim.OverPack
 
 class Footer extends Component {
   state = {
-    wechatOpen: false
+    wechatOpen: false,
+    pgpOpen: false
   }
 
   render() {
@@ -92,8 +94,8 @@ class Footer extends Component {
                 toggle={() =>
                   this.setState({ wechatOpen: !this.state.wechatOpen })
                 }
-                innerClassName="wechat-tooltip"
-                arrowClassName="wechat-tooltip-arrow"
+                innerClassName="footer-tooltip wechat-tooltip"
+                arrowClassName="footer-tooltip-arrow"
                 autohide={false}
               >
                 <img
@@ -102,6 +104,43 @@ class Footer extends Component {
                   width={120}
                   height={120}
                 />
+              </Tooltip>
+            </TweenOne>
+            <TweenOne
+              component="span"
+              key="2"
+              animation={{
+                opacity: 0,
+                type: 'from',
+                delay: this.props.delay + 500
+              }}
+              className="footer-icon"
+            >
+              <MdLock id="pgp-icon" size={18} />
+              <Tooltip
+                placement="top"
+                autohide={false}
+                isOpen={this.state.pgpOpen}
+                target="pgp-icon"
+                toggle={() => this.setState({ pgpOpen: !this.state.pgpOpen })}
+                innerClassName="footer-tooltip pgp-tooltip"
+                arrowClassName="footer-tooltip-arrow"
+              >
+                <div style={{ padding: '10px' }}>
+                  <FaKey size={48} color={'#ccc'} />
+                  <div style={{ textAlign: 'left', marginTop: '10px' }}>
+                    If you are security paranoid (which is a good thing of
+                    course), you can send me encrypted messages using my PGP key{' '}
+                    <a href="https://pgp.mit.edu/pks/lookup?op=get&search=0x3B2DB4E7">
+                      <span className="pgp-text">0x3B2DB4E7</span>
+                    </a>
+                    . The fingerprint is{' '}
+                    <span className="pgp-text">
+                      0403 2E39 8D6D 01AD B94B E122 E8FA DCBE 3B2D B4E7
+                    </span>
+                    .
+                  </div>
+                </div>
               </Tooltip>
             </TweenOne>
           </div>
