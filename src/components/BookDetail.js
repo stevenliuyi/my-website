31f22a05dec6521b7e0546e26baefef5fb7c1e7a
stevenliuyi <stevenliuyi@gmail.com>
2018-09-27 14:03:13 -0400

    add Goodreads and Wikidata links

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
+import { Tooltip } from 'reactstrap'
 
 class BookDetail extends Component {
+  state = {
+    tooltips: {
+      douban: false,
+      goodreads: false,
+      wikidata: false
+    }
+  }
+
+  tooltipToggle = link => {
+    let tooltips = this.state.tooltips
+    tooltips[link] = !tooltips[link]
+    this.setState({ tooltips })
+  }
+
   render() {
     return (
       <div>
@@ -14,15 +29,6 @@ class BookDetail extends Component {
               width={285}
               height={399}
             />
-            {this.props.douban && (
-              <a
-                href={`https://book.douban.com/subject/${this.props.douban}/`}
-                className="douban"
-                target="_blank"
-              >
-                豆
-              </a>
-            )}
           </div>
           <div className="book-detail-info">
             <div className="book-detail-title">{this.props.name}</div>
@@ -36,6 +42,76 @@ class BookDetail extends Component {
                     {tag}
                   </span>
                 ))}
+            </div>
+            <div className="book-detail-links">
+              {this.props.douban && (
+                <div id="douban" className="book-detail-link">
+                  <a
+                    href={`https://book.douban.com/subject/${
+                      this.props.douban
+                    }`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="iconfont icon-douban" />
+                  </a>
+                  <Tooltip
+                    placement="top"
+                    target="douban"
+                    isOpen={this.state.tooltips.douban}
+                    toggle={() => this.tooltipToggle('douban')}
+                  >
+                    Douban
+                  </Tooltip>
+                </div>
+              )}
+              {this.props.goodreads && (
+                <div id="goodreads" className="book-detail-link">
+                  <a
+                    href={`https://www.goodreads.com/book/show/${
+                      this.props.goodreads
+                    }`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="iconfont icon-goodreads" />
+                  </a>
+                  <Tooltip
+                    placement="top"
+                    target="goodreads"
+                    isOpen={this.state.tooltips.goodreads}
+                    toggle={() => this.tooltipToggle('goodreads')}
+                  >
+                    Goodreads
+                  </Tooltip>
+                </div>
+              )}
+              {this.props.wikidata && (
+                <div id="wikidata" className="book-detail-link">
+                  <a
+                    href={`https://www.wikidata.org/wiki/${
+                      this.props.wikidata
+                    }`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      alt="wikidata"
+                      src="/images/wikidata.svg"
+                      width={16}
+                      height={16}
+                    />
+                  </a>
+                  <Tooltip
+                    placement="top"
+                    target="wikidata"
+                    isOpen={this.state.tooltips.wikidata}
+                    toggle={() => this.tooltipToggle('wikidata')}
+                  >
+                    Wikidata
+                  </Tooltip>
+                </div>
+              )}
             </div>
           </div>
         </div>
